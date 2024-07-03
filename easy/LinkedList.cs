a30fb3053123a5a0f03ad7fd98c402d8fedab7a8
khoaluu2014 <khoaluu2014@gmail.com>
2024-07-03 14:51:32 -0700

    completed some LinkedList

--- easy/LinkedList.cs
+++ easy/LinkedList.cs
@@ -27,5 +27,22 @@ public class Solution
         return prev;
     }
 
-    public ListNode MergeTwoLists(ListNode list1, ListNode list2) { }
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+
+        if (list1.val < list2.val)
+        {
+            list1.next = MergeTwoLists(list1.next, list2);
+            return list1;
+        }
+        else
+        {
+            list2.next = MergeTwoLists(list2.next, list1);
+            return list2;
+        }
+    }
 }
