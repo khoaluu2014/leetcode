7ee8b7bd25b9c7a0f946f4829213a2af2ea8bb3a
khoaluu2014 <khoaluu2014@gmail.com>
2024-07-06 16:04:08 -0700

    finished more linked list

--- medium/LinkedList.cs
+++ medium/LinkedList.cs
@@ -10,6 +10,20 @@ public class ListNode
     }
 }
 
+public class Node
+{
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int _val)
+    {
+        val = _val;
+        next = null;
+        random = null;
+    }
+}
+
 public class Solution
 {
     public void ReorderList(ListNode head)
@@ -66,5 +80,51 @@ public class Solution
 
         slow.next = slow.next.next;
         return dummy.next;
+    }
+
+    public Node copyRandomList(Node head)
+    {
+        Dictionary<Node, Node> oldToCopy = new Dictionary<Node, Node>();
+
+        Node cur = head;
+        while (cur != null)
+        {
+            Node copy = new Node(cur.val);
+            oldToCopy[cur] = copy;
+            cur = cur.next;
+        }
+
+        cur = head;
+        while (cur != null)
+        {
+            Node copy = oldToCopy[cur];
+            copy.next = cur.next != null ? oldToCopy[cur.next] : null;
+            copy.random = cur.random != null ? oldToCopy[cur.random] : null;
+            cur = cur.next;
+        }
+
+        return head != null ? oldToCopy[head] : null;
+    }
+
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode ans = new ListNode();
+        ListNode cur = ans;
+
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int v1 = (l1 != null) ? l1.val : 0;
+            int v2 = (l2 != null) ? l2.val : 0;
+            int val = v1 + v2 + carry;
+            carry = val / 10;
+            val = val % 10;
+            cur.next = new ListNode(val);
+            cur = cur.next;
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+
+        return ans.next;
     }
 }
