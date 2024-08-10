2c0963f387fe37dcd114c1e2de4665ff96ed1971
khoaluu <khoaluu2014@gmail.com>
2024-08-10 11:25:49 -0700

    finish heap

--- medium/Heap.cs
+++ medium/Heap.cs
@@ -82,6 +82,45 @@ public class Twitter
     }
 }
 
+public class MedianFinder {
+    
+  private PriorityQueue<int, int> minHeap;
+  private PriorityQueue<int, int> maxHeap;
+
+    public MedianFinder() {
+      minHeap = new PriorityQueue<int, int>();
+      maxHeap = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => b.CompareTo(a)));
+    }
+    
+    public void AddNum(int num) {
+      if(minHeap.Count != 0 && num > minHeap.Peek()) {
+        minHeap.Enqueue(num, num);
+      }
+      else {
+        maxHeap.Enqueue(num, num);
+      }
+
+      if(minHeap.Count > maxHeap.Count + 1) {
+        int val = minHeap.Dequeue();
+        maxHeap.Enqueue(val, val);
+      }
+      else if(maxHeap.Count > minHeap.Count + 1) {
+        int val = maxHeap.Dequeue();
+        minHeap.Enqueue(val, val);
+      }
+    }
+    
+    public double FindMedian() {
+      if(minHeap.Count > maxHeap.Count) {
+        return minHeap.Peek();
+      }
+      else if(maxHeap.Count > minHeap.Count) {
+        return maxHeap.Peek();
+      }
+        return (minHeap.Peek() + maxHeap.Peek()) / 2.0;
+    }
+}
+
 public class Heap
 {
     public int LastStoneWeight(int[] stones)
