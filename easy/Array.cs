9329aaa211e842780a74fc8cc4755a7881a4f55c
khoaluu2014 <khoaulu2014@gmail.com>
2025-05-01 13:59:50 -0700

    finish easy Array

--- easy/Array.cs
+++ easy/Array.cs
@@ -45,4 +45,19 @@ public class Array
         }
         return true;
     }
+
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int value = target - nums[i];
+            if (dict.ContainsKey(value))
+            {
+                return new int[] { dict[value], i };
+            }
+            dict.Add(nums[i], i);
+        }
+        return new int[] { -1, -1 };
+    }
 }
